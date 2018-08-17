d19a4c1272aaacc39b4958d9d198dd4786c6b16e
Paola Mendiburu <paolamendiburualetti@gmail.com>
2018-08-17 14:32:22 +0200

    Anadimos el modelo __str__ para que en el admin de Django aparezcan correctamente los titulos de los post

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     introduction = models.TextField()
     full_text = models.TextField()
-    image_url = models.URLField(null=True, blank=True)
+    image_url = models.FileField(null=True, blank=True)
     publication_date = models.DateTimeField()
     created_on = models.DateTimeField(auto_now_add=True)
+
+
+def __str__(self):
+    """
+    Define cómo se representa un Post como una string
+    """
+    return '{0})'.format(self.title)
